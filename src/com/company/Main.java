01fd5e0ca1d0b527434bbbae1bb52355ce4c32a8
KennyWagner <wagnerkenneth72@gmail.com>
2022-04-13 18:17:36 -0400

    continue playing edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,22 +8,24 @@ public class Main
         String player1;
         String player2;
         Scanner in = new Scanner(System.in);
+        String playResponse;
+        boolean keepPlaying = true;
 
-        while (true)
+        while (keepPlaying)
         {
             System.out.println();
             System.out.println("NEW GAME");
             //player 1 input
             do
             {
-                System.out.println("Input your choice player 1 (R, P, or S)");
+                System.out.println("Input your choice player 1 [R, P, or S]");
                 player1 = in.nextLine();
             } while (!(player1.equalsIgnoreCase("R") || player1.equalsIgnoreCase("P") || player1.equalsIgnoreCase("S")));
 
             //player 2 input
             do
             {
-                System.out.println("Input your choice player 2 (R, P, or S)");
+                System.out.println("Input your choice player 2 [R, P, or S]");
                 player2 = in.nextLine();
             } while (!(player2.equalsIgnoreCase("R") || player2.equalsIgnoreCase("P") || player2.equalsIgnoreCase("S")));
 
@@ -73,6 +75,13 @@ public class Main
                 {
                     System.out.println("Scissors ties with Scissors, it's a tie!");
                 }
+            }
+
+            System.out.println("Do you want to keep playing: [Y/N]");
+            playResponse = in.nextLine();
+            if (playResponse.equalsIgnoreCase("N"))
+            {
+                System.exit(0);
             }
         }
     }
